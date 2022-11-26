686afacb350f528b0398fdc3d8ed7bc262b33899
Leandro <lafs.dev@gmail.com>
2022-11-26 20:27:03 -0300

    Criado a entidade cidade

--- src/main/java/io/github/lafsdev/localizacao/LocalizacaoApplication.java
+++ src/main/java/io/github/lafsdev/localizacao/LocalizacaoApplication.java
@@ -13,6 +13,6 @@ public class LocalizacaoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		System.out.println("Inicializando");
 	}
 }
---
+++ src/main/java/io/github/lafsdev/localizacao/domain/entity/Cidade.java
@@ -0,0 +1,22 @@
+package io.github.lafsdev.localizacao.domain.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table(name = "tb_cidade")
+public class Cidade {
+
+    @Id
+    @Column(name = "id_cidade")
+    private Long id;
+    @Column(name = "nome", length = 50)
+    private String nome;
+    @Column(name = "qtd_habitantes")
+    private Long habitantes;
+}
