f9a0d82293f717929dc5aae5da526cc7ace3f209
Leandro <lafs.dev@gmail.com>
2022-12-02 22:21:32 -0300

    Query methods com like

--- src/main/java/io/github/lafsdev/localizacao/domain/repository/CidadeRepository.java
+++ src/main/java/io/github/lafsdev/localizacao/domain/repository/CidadeRepository.java
@@ -2,6 +2,7 @@ package io.github.lafsdev.localizacao.domain.repository;
 
 import io.github.lafsdev.localizacao.domain.entity.Cidade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -19,6 +20,10 @@ public interface CidadeRepository extends JpaRepository<Cidade, Long> {
 
     //Contendo aquele pedaço
     List<Cidade> findByNomeContaining(String nome);
+
+    // Busca pelo nome like
+    @Query("select c from Cidade c where upper(c.nome) like upper(?1)")
+    List<Cidade> findByNomeLike(String nome);
 
     List<Cidade> findByHabitantes(Long habitantes);
 
